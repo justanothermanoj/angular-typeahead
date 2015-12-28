72caada922a3985e7b218e51af1abfde5f231e97
justanothermanoj <justanothermanoj@gmail.com>
2015-12-28 09:50:04 +0530

    Updated sorting algorithm

--- js/app.js
+++ js/app.js
@@ -18,12 +18,12 @@ app.controller('angular-typeahead', function($scope) {
 
     $scope.autocomplete = function() {
         if ($scope.filteredTerms[0]) {
-        	var first = $scope.filteredTerms[0];
-        	var input = $scope.input;
-            if (first.substr(0,input.length).toLowerCase() == input.toLowerCase()) {
-            	$scope.first_term = first.replace(first.substr(0,input.length), input);
+            var first = $scope.filteredTerms[0];
+            var input = $scope.input;
+            if (first.substr(0, input.length).toLowerCase() == input.toLowerCase()) {
+                $scope.first_term = first.replace(first.substr(0, input.length), input);
             } else {
-            	$scope.first_term = "";
+                $scope.first_term = "";
             }
         } else {
             $scope.first_term = "";
@@ -49,4 +49,44 @@ app.filter('highlight', function($sce) {
         if (phrase) text = text.replace(new RegExp('(' + phrase + ')', 'gi'), '<span class="highlighted">$1</span>');
         return $sce.trustAsHtml(text);
     }
+});
+
+
+app.filter('sort', function() {
+
+    function CustomOrder(term, input) {
+        var value = 0;
+
+        if (term.substr(0, input.length).toLowerCase() == input.toLowerCase()) {
+            value = 1;
+        }
+
+        return value;
+    }
+
+    return function(items, field) {
+        var filtered = [];
+        if (field[2] == 'true') {
+            angular.forEach(items, function(item) {
+                if (item.substr(0, field[1].length).toLowerCase() == field[1].toLowerCase()) {
+                    filtered.push(item);
+                }
+            });
+        } else {
+            angular.forEach(items, function(item) {
+                filtered.push(item);
+            });
+        }
+        filtered.sort(function(a, b) {
+
+            if (CustomOrder(a, field[1]) > CustomOrder(b, field[1])) {
+                return -1;
+            }
+            if (CustomOrder(a, field[1]) < CustomOrder(b, field[1])) {
+                return 1;
+            }
+            return 0;
+        });
+        return filtered;
+    };
 });
