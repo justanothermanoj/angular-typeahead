9f8c95c6256225e62ddc9da49976224fb7a8c894
justanothermanoj <justanothermanoj@gmail.com>
2016-01-04 20:00:20 +0530

    added directive attribute options

--- js/app.js
+++ js/app.js
@@ -26,20 +26,26 @@ app.directive('angularTypeahead', function() {
         scope.recent = -1;
         scope.pre_term = '';
         // array of data to search from
-        scope.data = [
-            'Youtube',
-            'Twitter',
-            'Linkedin',
-            'Github',
-            'Soundcloud',
-            'Twitch',
-            'Vimeo',
-            'Facebook',
-            'Google',
-            'Duckduckgo',
-            'Spotify',
-            'Google+'
-        ];
+        if (typeof scope.data === 'undefined') {
+            scope.data = [
+                'Youtube',
+                'Twitter',
+                'Linkedin',
+                'Github',
+                'Soundcloud',
+                'Twitch',
+                'Vimeo',
+                'Facebook',
+                'Google',
+                'Duckduckgo',
+                'Spotify',
+                'Google+'
+            ];
+        }
+
+        if (typeof scope.limit === 'undefined') {
+            scope.limit = 10;
+        }
 
         element.bind("keydown keypress", function(event) {
             if (event.which === 40) {
@@ -58,8 +64,8 @@ app.directive('angularTypeahead', function() {
                 scope.$apply(function() {
                     if (scope.recent <= 0) {
                         $('#pre-term').hide();
-                            scope.recent = -1;
-                            scope.set_pre_term('');
+                        scope.recent = -1;
+                        scope.set_pre_term('');
                     } else {
                         $('#pre-term').show();
                         scope.recent = scope.recent - 1;
@@ -70,7 +76,7 @@ app.directive('angularTypeahead', function() {
                 event.preventDefault();
             } else if (event.which === 13) {
                 scope.$apply(function() {
-                    if(scope.pre_term != '') {
+                    if (scope.pre_term != '') {
                         scope.input = scope.pre_term;
                     } else if (scope.first_term != '') {
                         scope.set_term(scope.first_term);
@@ -83,7 +89,7 @@ app.directive('angularTypeahead', function() {
 
         scope.autocomplete = function() {
             $("#angular-typehead-results").show();
-            if(scope.first_term == scope.input && scope.input != '' && !(scope.filteredTerms.length > 1)) {
+            if (scope.first_term == scope.input && scope.input != '' && !(scope.filteredTerms.length > 1)) {
                 $("#angular-typehead-results").hide();
             }
             scope.recent = -1;
@@ -105,11 +111,11 @@ app.directive('angularTypeahead', function() {
             }
         }
 
-        scope.set_pre_term = function (text) {
+        scope.set_pre_term = function(text) {
             scope.pre_term = text;
         }
 
-        scope.set_term = function (text) {
+        scope.set_term = function(text) {
             scope.input = text;
         }
 
@@ -117,7 +123,6 @@ app.directive('angularTypeahead', function() {
 
         scope.$watch('input', function(newval, oldval) {
 
-            $('#angular-typehead-autocomplete-term').show();
             $('#pre-term').hide();
             scope.autocomplete();
 
@@ -135,7 +140,10 @@ app.directive('angularTypeahead', function() {
     return {
         restrict: 'E',
         link: link,
-        scope: {},
+        scope: {
+            data: '=',
+            limit : '='
+        },
         templateUrl: 'templates/default.html'
     }
 });
@@ -144,7 +152,9 @@ app.directive('angularTypeahead', function() {
 // Main Controller
 
 app.controller('typeahead-controller', function($scope) {
-
+    $scope.countries = [
+        "Afghanistan", "Aland Islands", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Barbuda", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Trty.", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Caicos Islands", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "French Guiana", "French Polynesia", "French Southern Territories", "Futuna Islands", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard", "Herzegovina", "Holy See", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Jan Mayen Islands", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea", "Korea (Democratic)", "Kuwait", "Kyrgyzstan", "Lao", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macao", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "McDonald Islands", "Mexico", "Micronesia", "Miquelon", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "Nevis", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Palestinian Territory, Occupied", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Principe", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Barthelemy", "Saint Helena", "Saint Kitts", "Saint Lucia", "Saint Martin (French part)", "Saint Pierre", "Saint Vincent", "Samoa", "San Marino", "Sao Tome", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia", "South Sandwich Islands", "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "The Grenadines", "Timor-Leste", "Tobago", "Togo", "Tokelau", "Tonga", "Trinidad", "Tunisia", "Turkey", "Turkmenistan", "Turks Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "US Minor Outlying Islands", "Uzbekistan", "Vanuatu", "Vatican City State", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (US)", "Wallis", "Western Sahara", "Yemen", "Zambia", "Zimbabwe"
+    ];
 });
 
 // filter to highlight results
